f029ec459053dd64b7f71f4d2bcb27063e971c58
dhivakarfulldev <dhivakarfulldev@gmail.com>
2026-09-24 23:09:12 +0530

    files updated

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors"
 
 dotenv.config();
-
+app.use(cors())
 const app = express();
 app.use(express.json())
 let users = [
